4dc2d1df9acdd43f37f24f1d18637f45d730cd3f
Farhan Solih <bebekkeju21@gmail.com>
2025-05-09 00:50:16 +0700

    tambah util

--- app/src/main/java/com/farhansolih0009/assesment02/MainActivity.kt
+++ app/src/main/java/com/farhansolih0009/assesment02/MainActivity.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.farhansolih0009.assesment02.navigation.SetupNavGraph
 import com.farhansolih0009.assesment02.ui.theme.Assesment02Theme
+import com.farhansolih0009.assesment02.navigation.SetupNavGraph
+import com.farhansolih0009.assesment02.ui.theme.Assesment02Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +16,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Assesment02Theme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SetupNavGraph()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Assesment02Theme {
-        Greeting("Android")
     }
 }
